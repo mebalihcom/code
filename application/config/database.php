8d6f28514f2596e4e26b019dc9d2a5d630f2cf49
gunsu12 <gunawansuarna@gmail.com>
2017-11-26 07:14:25 +0800

    ubah konfigurasi database dan routes
    tes tes

--- application/config/database.php
+++ application/config/database.php
@@ -49,10 +49,10 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'postgres';
-$db['default']['password'] = 'password';
-$db['default']['database'] = 'mebalih';
-$db['default']['dbdriver'] = 'postgre';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
+$db['default']['database'] = 'db_mebalih';
+$db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
